fdfe338d8e8a3814f401450c934fa28cf4aec09d
stevedeck <stephen.deck@gmail.com>
2023-04-10 13:42:25 -0600

    updated output in methods to see if it makes a difference

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -3,17 +3,17 @@ def create_favorite_hash(color, number)
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
   my_hash = {:color => color, :number => number}
-  p my_hash
+  return my_hash
 end
 
 def favorite_color(favorite_list)
   # return the value of the color key
-  p favorite_list[:color]
+  return favorite_list[:color]
 end
 
 def favorite_number(favorite_list)
   # use #fetch to return the value of the number key or 42 if the key is not found
-  favorite_list.fetch(:number, 42)
+  return favorite_list.fetch(:number, 42)
 end
 
 def update_favorite_movie(favorite_list, movie)
@@ -28,7 +28,7 @@ def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
   favorite_list.delete(:number)
   # Step 2: return the hash (because Step 1 returns the value of the number key)
-  p favorite_list
+  return favorite_list
 end
 
 def favorite_categories(favorite_list)
