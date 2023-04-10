e099843dbf05e1d94c2f605982461b12bfee3234
stevedeck <stephen.deck@gmail.com>
2023-04-10 15:18:37 -0600

    updates to methods exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,22 +7,35 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+    return number.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
-
+def  common_sports(current, favorite)
+    return current.intersection(current, favorite)
+end
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+    return games.uniq.sort
+end
+
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+
+def lucky_number(number=7)
+    return "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -31,6 +44,12 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(character)
+    if(character.length != 1)
+        return 'Input Error';
+    end
+    return character.ord;
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
